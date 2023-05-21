52f805a89cc2e778d97e04b3f869b8a3a29ec242
viinnneeeet <poojaryvineeet@gmail.com>
2023-05-21 10:36:32 +0530

    netlify build deploy setup

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,57 +1,57 @@
-const nodemailer = require('nodemailer');
-const bcrypt = require('bcrypt');
-const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
-const UserOtp = require('../models/user.otp.model');
+// const nodemailer = require('nodemailer');
+// const bcrypt = require('bcrypt');
+// const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+// const UserOtp = require('../models/user.otp.model');
 
-let transporter = nodemailer.createTransport({
-  host: 'smtp.ethereal.email',
-  auth: {
-    user: 'lottie80@ethereal.email',
-    pass: 'SnSgJcTnG9wE5HChh4',
-  },
-});
-// verify connection configuration
+// let transporter = nodemailer.createTransport({
+//   host: 'smtp.ethereal.email',
+//   auth: {
+//     user: 'lottie80@ethereal.email',
+//     pass: 'SnSgJcTnG9wE5HChh4',
+//   },
+// });
+// // verify connection configuration
 
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take our messages');
-  }
-});
+// transporter.verify(function (error, success) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Server is ready to take our messages');
+//   }
+// });
 
-const sendOTPVerificationEmail = catchAsyncErrors(
-  async (_id, email, res, options) => {
-    const otp = `${Math.floor(1000 + Math.random() * 9000)}`;
+// const sendOTPVerificationEmail = catchAsyncErrors(
+//   async (_id, email, res, options) => {
+//     const otp = `${Math.floor(1000 + Math.random() * 9000)}`;
 
-    //mail options
-    const mailOptions = {
-      form: 'lottie80@ethereal.email',
-      to: email,
-      subject: 'Verify Email',
-      html: `<p>Enter ${otp} in the app to verify your email address and complete the signup </p>
-    <p>This code <b>expires in 2 mins</b></p>`,
-      text: `Enter ${otp} in the aoo to verify your email address and complete the signup`,
-    };
-    //hash the otp
-    const saltRounds = 10;
-    const hashedOtp = await bcrypt.hash(otp, saltRounds);
+//     //mail options
+//     const mailOptions = {
+//       form: 'lottie80@ethereal.email',
+//       to: email,
+//       subject: 'Verify Email',
+//       html: `<p>Enter ${otp} in the app to verify your email address and complete the signup </p>
+//     <p>This code <b>expires in 2 mins</b></p>`,
+//       text: `Enter ${otp} in the aoo to verify your email address and complete the signup`,
+//     };
+//     //hash the otp
+//     const saltRounds = 10;
+//     const hashedOtp = await bcrypt.hash(otp, saltRounds);
 
-    const newOtpVerify = new UserOtp({
-      userId: _id,
-      otp: hashedOtp,
-      createdAt: Date.now(),
-      expiresAt: Date.now() + 120000,
-    });
-    await newOtpVerify.save();
-    transporter.sendMail(mailOptions);
+//     const newOtpVerify = new UserOtp({
+//       userId: _id,
+//       otp: hashedOtp,
+//       createdAt: Date.now(),
+//       expiresAt: Date.now() + 120000,
+//     });
+//     await newOtpVerify.save();
+//     transporter.sendMail(mailOptions);
 
-    return res.json({
-      success: true,
-      message: 'Otp sent',
-      otp,
-    });
-  }
-);
+//     return res.json({
+//       success: true,
+//       message: 'Otp sent',
+//       otp,
+//     });
+//   }
+// );
 
-module.exports = sendOTPVerificationEmail;
+// module.exports = sendOTPVerificationEmail;
